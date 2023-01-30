d8a6291b6ffb0a599f0ee784759cdb950b0fe0bc
Cieprojets <zineb.fahim@epsi.fr>
2023-01-30 13:44:37 +0100

    Initial commit

---
+++ src/main/java/fr/epsi/b3/c1/g1/projetmspr/ProjetMsprApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.b3.c1.g1.projetmspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMsprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/b3/c1/g1/projetmspr/ProjetMsprApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.b3.c1.g1.projetmspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMsprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
